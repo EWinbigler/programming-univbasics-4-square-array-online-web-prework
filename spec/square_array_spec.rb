07c9c02890671145e083b67974b1405bbb236331
Eric Winbigler <uzof99@gmail.com>
2019-06-26 14:44:48 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -2,14 +2,7 @@ require_relative './spec_helper'
 
 describe '#square_array' do
 
-  it 'does not call on each, collect, map, or inject' do
-    numbers = [1,2,3]
-    expect(numbers).to_not receive(:each)
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
-    square_array(numbers)
-  end
+  
   
   
 
