5eec9efde2cb5524e5cfeafcd716d3feba8809fb
Eric Winbigler <uzof99@gmail.com>
2019-06-26 14:30:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
  squared=[]
-  array.each do |number|
-    squared << number * number
-  end
-  return squared
+array.each do |element|
+squared << element ** 2 
+end
+squared
 end
